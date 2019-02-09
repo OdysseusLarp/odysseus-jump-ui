4a223ece6a7505644d1bef7dce1342a79b930ffd
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-02-09 12:15:14 +0200

    feat: add functionality for scanning unscanned objects

--- src/app/components/object-details/object-details.component.ts
+++ src/app/components/object-details/object-details.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { BehaviorSubject, Subscription } from 'rxjs';
+import { Subscription, interval, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { StateService } from '@app/services/state.service';
 import { getFeatureProperties } from '@components/map/map.component';
+import { get, set } from 'lodash';
+import * as moment from 'moment';
+import { putEvent } from '@api/Event';
 
 @Component({
 	selector: 'app-object-details',
@@ -10,8 +14,11 @@ import { getFeatureProperties } from '@components/map/map.component';
 })
 export class ObjectDetailsComponent implements OnInit, OnDestroy {
 	selectedFeature$: Subscription;
+	events$: Subscription;
 	feature: any;
 	formattedProperties: any;
+	isScanning = false;
+	scanEvent: api.Event;
 
 	constructor(private state: StateService) {}
 
@@ -23,9 +30,60 @@ export class ObjectDetailsComponent implements OnInit, OnDestroy {
 				key => `${key}: ${props[key]}`
 			);
 		});
+		this.events$ = combineLatest(this.state.events, interval(1000))
+			.pipe(
+				map(([events]) => {
+					// Add human readable seconds until scan completes
+					return events.map(event => ({
+						...event,
+						occurs_in_seconds: moment(event.occurs_at).diff(
+							moment(),
+							'seconds'
+						),
+					}));
+				})
+			)
+			.subscribe(events => {
+				const featureId = get(this.feature, 'properties.id');
+				if (!featureId) return;
+				const scanEvent = events
+					.filter(event => event.type === 'SCAN_OBJECT')
+					.find(event => get(event, 'metadata.target') === featureId);
+				if (scanEvent) this.setScanEvent(scanEvent);
+				else if (!scanEvent && this.scanEvent) this.finishScanEvent();
+			});
+	}
+
+	private setScanEvent(event) {
+		this.scanEvent = event;
+		this.isScanning = true;
+	}
+
+	private finishScanEvent() {
+		this.scanEvent = undefined;
+		this.isScanning = false;
+		// TODO: Refetch feature instead of doing this dirty stuff
+		if (this.feature) set(this.feature, 'properties.is_scanned', true);
 	}
 
 	ngOnDestroy() {
 		this.selectedFeature$.unsubscribe();
+		this.events$.unsubscribe();
+	}
+
+	onSendProbeClick() {
+		console.log('Sending probe to', this.feature);
+		const id = get(this.feature, 'properties.id');
+		const scanTime = moment()
+			.add(30, 'seconds') // 30 second scan time hardcoded for testing
+			.format();
+		putEvent({
+			type: 'SCAN_OBJECT',
+			ship_id: 'odysseus',
+			is_active: true,
+			occurs_at: scanTime,
+			metadata: { target: id },
+			status: 'Meh', // TODO: Drop status field alltogether?
+		});
 	}
 }
